d2bd3e6947a99ee268ac670c4ccc95ddc7935c61
SethupathyD <sethupathy.d@syncfusion.com>
2025-03-21 15:16:07 +0530

    code updated.

---
+++ SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Syncfusion.Maui.DataGrid;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            sfGrid.CellRenderers.Add("TextImage", new TextImageColumnRenderer());
+        }
+    }
+}
